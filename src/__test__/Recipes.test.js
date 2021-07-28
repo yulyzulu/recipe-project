7c5048a3dc406c2c5dab2ca46f2296a93ba1a9a5
Yulieth Zuluaga <yulyzulu05@gmail.com>
2021-07-28 17:59:48 -0500

    Change directories

--- src/__test__/Recipes.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Recipes from '../components/Recipes';
-
-test('Render h2 element', () => {
-  render(<Recipes />);
-  expect(screen.getByText('Recipes')).toBeInTheDocument();
-
-});
